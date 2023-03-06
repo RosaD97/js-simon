d2261c40a114b67b62e815bd6026d53c2841dab7
RosaD97 <r960595@gmail.com>
2023-03-06 15:40:49 +0100

    numeri che scompaiono

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// Funzione Genera numero random
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// Dopo un tempo impostato i numeri scompaiono
+setTimeout(timerNumeri, 3000);
+function timerNumeri() {
+    numbers.innerHTML = 'indovina';
+}
+
+// Main
+const numbers = document.querySelector('.ms_numbers');
+const numbersG = [];
+
+while (numbersG.length <= 5) {
+    const nGenerati = getRandomInt(50);
+    numbersG.push(nGenerati);
+}
+
+numbers.innerHTML = numbersG;
